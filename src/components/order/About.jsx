eda454bb64932fe2abdeed6a36c951340f520044
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-12-09 20:22:38 +0700

    fix(component): fix about.jsx

--- src/components/order/About.jsx
+++ src/components/order/About.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-import ListItem from "./listItem";
+import ListItem from "./ListItem";
 
 function About({ services, otherService, whyChoseus }) {
   return (
     <div className="mb-[113px] max-w-[1219px] text-justify">
       <h3 className="font-bold text-black mb-[10px]">Tentang Gig Ini</h3>
       <p>
-        Apakah Anda membutuhkan jasa tukang untuk memperbaiki perabotan rumah
-        tangga yang rusak? Atau mungkin Anda ingin melakukan pemeliharaan rutin
-        agar perabotan tetap dalam kondisi terbaik? Anda berada di tempat yang
-        tepat! Dengan pengalaman lebih dari sepuluh tahun dalam bidang perbaikan
-        dan pemeliharaan perabotan rumah tangga, saya siap membantu Anda dengan
-        berbagai layanan servis untuk kebutuhan rumah tangga Anda.
+        Apakah Anda membutuhkan jasa tukang untuk memperbaiki perabotan rumah tangga yang rusak? Atau mungkin Anda ingin melakukan pemeliharaan rutin agar perabotan tetap dalam kondisi terbaik? Anda berada di tempat yang tepat! Dengan
+        pengalaman lebih dari sepuluh tahun dalam bidang perbaikan dan pemeliharaan perabotan rumah tangga, saya siap membantu Anda dengan berbagai layanan servis untuk kebutuhan rumah tangga Anda.
       </p>
-      <h5 className="font-bold text-black mt-[30px]">
-        Layanan yang kami tawarkan
-      </h5>
+      <h5 className="font-bold text-black mt-[30px]">Layanan yang kami tawarkan</h5>
 
       <ListItem items={services} />
-      <h5 className="font-bold text-black">
-        Kami juga menyediakan jasa tambahan seperti:
-      </h5>
+      <h5 className="font-bold text-black">Kami juga menyediakan jasa tambahan seperti:</h5>
       <ListItem items={otherService} />
       <h5 className="font-bold text-black">Kenapa Memilih Kami:</h5>
       <ListItem items={whyChoseus} />
       <br />
-      <p className="text-[#444444]">
-        Hubungi saya terlebih dahulu sebelum memesan. Saya ingin mendengar
-        kebutuhan Anda dan memberikan solusi yang tepat!
-      </p>
+      <p className="text-[#444444]">Hubungi saya terlebih dahulu sebelum memesan. Saya ingin mendengar kebutuhan Anda dan memberikan solusi yang tepat!</p>
     </div>
   );
 }
